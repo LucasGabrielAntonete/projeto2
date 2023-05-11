00414d889a223c08e9de8d31b937df423277d013
LucasGabrielAntonete <lucasantonete@gmail.com>
2023-05-11 12:12:05 -0300

    criação de produtos

---
+++ src/produtos.js
@@ -0,0 +1,9 @@
+export const items = [
+    { id: 1, name: 'Camisa', details: { description: 'Camisa Social do Bolsonaro', price: 50.00 }},
+    { id: 2, name: 'Calça', details: { description: 'Calça super confortavel', price: 20.00}},
+    { id: 3, name: 'Meia', details: { description: 'Meia Patriota', price: 20.00}},
+    { id: 4, name: 'Caneca', details: { description: 'Caneca DO MITO', price: 20.00}},
+    { id: 5, name: 'Calendario', details: { description: 'Calendario do Capitão', price: 20.00}},
+    { id: 6, name: 'Toalha', details: { description: 'Toalha Patriota', price: 20.00}},
+
+]
