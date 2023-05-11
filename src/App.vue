e20483f435c1fe999ca84d86db29522b023972a4
LucasGabrielAntonete <lucasantonete@gmail.com>
2023-05-11 15:37:54 -0300

    criação do sistema de quantidade, ainda a fazer

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,33 @@
-<script setup></script>
-<template>
+<script setup>
+import items from '@/_data/produtos'
+import { ref } from 'vue'
+
+const carrinho = ref([]);
+
+function adicionarItem() {
+  const itemCarrinho = carrinho.value.items
+}
+
  
- <nav class="navbar navbar-dark bg-dark fixed-top">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">SUPER LOJA DO MITO </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
-      <div class="offcanvas-header">
-        <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Carrinho</h5>
-        <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-      </div>
-      <div class="offcanvas-body">
-        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-          <li class="nav-item">
-            <a class="nav-item">{{ item }}</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-item" >{{ item }}</a>
-          </li>
-        </ul>
-      </div>
+
+</script>
+<template>
+  <main>
+    <div>
+      <ul>
+        <li class="card" v-for="item in items" :key="item.id">
+          <div>
+            <h5>Nome: {{ item.name }}</h5>
+            <p>detalhes: {{ item.details }}</p>
+            <p>price:R$:{{ item.details.price }}</p>
+            <button class="bnt bnt-primary" @click="item.value.quantidade++">+</button>
+            <button @click="item.value.quantidade--">-</button>
+            <button @click="adicionarItem">Adicionar item</button>
+            <p>{{ item.quantidade }}</p>
+          </div>
+        </li>
+      </ul>
     </div>
-  </div>
-</nav>
-
+  </main>
 </template>
-<style scoped>
-
-</style>
+<style scoped></style>
---
+++ src/_data/produtos.js
@@ -0,0 +1,28 @@
+const items = [
+  {
+    id: 1,
+    name: 'Camisa',
+    details: { description: 'Camisa Social do Bolsonaro', price: 50.0 },
+    quantidade: 0
+  },
+  {
+    id: 2,
+    name: 'Calça',
+    details: { description: 'Calça super confortavel do Mito', price: 20.0 },
+    quantidade: 0
+  },
+  { id: 3, name: 'Meia', details: { description: 'Meia Patriota', price: 20.0 }, quantidade: 0 },
+  { id: 4, name: 'Caneca', details: { description: 'Caneca DO MITO', price: 20.0 }, quantidade: 0 },
+  {
+    id: 5,
+    name: 'Calendario',
+    details: { description: 'Calendario do Capitão', price: 20.0 },
+    quantidade: 0
+  },
+  { id: 6, 
+    name: 'Toalha', 
+    details: { description: 'Toalha Patriota', price: 20.0 }, 
+    quantidade: 0 }
+]
+
+export default items
