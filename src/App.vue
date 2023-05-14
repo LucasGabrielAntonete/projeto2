cfb5a8da5aedb3289a9fa1d90c8bacd6b770202f
LucasGabrielAntonete <lucasantonete@gmail.com>
2023-05-14 00:05:27 -0300

    Criação das funçoes e quase termino da parte logica

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,75 @@
 <script setup>
-import items from '@/_data/produtos'
+import produtos from '@/_data/produtos'
 import { ref } from 'vue'
 
 const mostrar = ref(true)
 
 const carrinho = ref({
   items: [],
-  
+
   valorTotal: 0
-});
+})
 
-
+function limparCarrinho() {
+  carrinho.value.items = []
+}
 
 function adicionarItem(item) {
-  let itemCarrinho = carrinho.value.items.find(produto => produto.id == item.id )
+  let itemCarrinho = carrinho.value.items.find((produto) => produto.id == item.id)
   if (itemCarrinho) {
-    itemCarrinho.quantidade += 1;
+    itemCarrinho.quantidade += 1
   } else {
     carrinho.value.items.push(item)
   }
-
 }
+
+function incrementaCompra(index) {
+  index.value.quantidade++
+}
+
+function decrementaCompra(index) {
+  if (index.value.quantidade <= 0) {
+    alert('Os itens ja foram retirados')
+  } else index.value.quantidade--
+}
+
+
+// Fazer a função de valor total e mexer no css e template// 
+
 </script>
 <template>
   <main>
-    <div>
-      <ul>
-        <li class="card" v-for="item in items" :key="item.id">
-          <div>
-            <h5>Nome: {{ item.name }}</h5>
-            <p>detalhes: {{ item.details.description }}</p>
-            <p>price:R$:{{ item.details.price }}</p>
-            <button class="" @click="item.quantidade++">+</button>
-            <button @click="item.quantidade--">-</button>
-            <button @click="adicionarItem(item)">Adicionar item</button>
-            <p>{{ item.quantidade }}</p>
-            <img src="" alt="">
+    <ul>
+      <li class="container" v-for="item in produtos" :key="item.id">
+        <div class="row">
+          <div class="col">
+            <div class="card">
+              <h5>Nome: {{ item.name }}</h5>
+              <p>detalhes: {{ item.details.description }}</p>
+              <p>price:R$:{{ item.details.price }}</p>
+              <button class="" @click="incrementaCompra(ref(item))">+</button>
+              <button @click="decrementaCompra(ref(item))">-</button>
+              <button @click="adicionarItem(item)">Adicionar item</button>
+              <p>{{ item.quantidade }}</p>
+              <img src="" alt="" />
+            </div>
           </div>
-        </li>
-      </ul>
-    </div>
+        </div>
+      </li>
+    </ul>
+
     <div>
-    <button @click="mostrar =! mostrar">Mostrar</button>
+      <button @click="mostrar =! mostrar">Mostrar Carrinho</button>
     <div v-if="mostrar">
-      {{ carrinho }}
+      <button @click="limparCarrinho()">Limpar Carrinho</button>
+      
+      <div v-for="(index, item) in carrinho.items" :key="item.id">
+        {{ index }}
+        <button @click="incrementaCompra(ref(index))">+</button>
+        <button @click="decrementaCompra(ref(index))">-</button>
+    
+     
+      </div>
     </div>
   </div>
   </main>
--- src/_data/produtos.js
+++ src/_data/produtos.js
@@ -1,33 +1,45 @@
 import { ref } from 'vue'
 
-
-
-const items = ref([
+const produtos = ref([
   {
     id: 1,
     name: 'Camisa',
     details: { description: 'Camisa Social do Bolsonaro', price: 50.0 },
-    quantidade: 0
+    quantidade: 0,
+   
   },
   {
     id: 2,
     name: 'Calça',
     details: { description: 'Calça super confortavel do Mito', price: 20.0 },
-    quantidade: 0
+    quantidade: 0,
+   
   },
-  { id: 3, name: 'Meia', details: { description: 'Meia Patriota', price: 20.0 }, quantidade: 0 },
-  { id: 4, name: 'Caneca', details: { description: 'Caneca DO MITO', price: 20.0 }, quantidade: 0 },
+  { id: 3,
+    name: 'Meia',
+    details: { description: 'Meia Patriota', price: 20.0 },
+    quantidade: 0,
+    
+      },
+  { id: 4,
+    name: 'Caneca',
+    details: { description: 'Caneca DO MITO', price: 20.0 },
+    quantidade: 0,
+    
+       },
   {
     id: 5,
     name: 'Calendario',
     details: { description: 'Calendario do Capitão', price: 20.0 },
-    quantidade: 0
+    quantidade: 0,
+   
   },
-  { id: 6, 
-    name: 'Toalha', 
-    details: { description: 'Toalha Patriota', price: 20.0 }, 
-    quantidade: 0 }
-
+  { 
+    id: 6,
+    name: 'Toalha',
+    details: { description: 'Toalha Patriota', price: 20.0 },
+    quantidade: 0,
+     },
 ])
 
-export default items
+export default produtos
