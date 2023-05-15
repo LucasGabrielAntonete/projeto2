de1f581ecef89dd53f85aba46ed7ec55c82e3a9f
LucasGabrielAntonete <lucasantonete@gmail.com>
2023-05-14 22:18:33 -0300

    commit final

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,20 @@
 import produtos from '@/_data/produtos'
 import { ref } from 'vue'
 
-const mostrar = ref(true)
+const mostrar = ref(false)
 
 const carrinho = ref({
-  items: [],
-
-  valorTotal: 0
+  items: []
 })
+
 
 function limparCarrinho() {
   carrinho.value.items = []
+}
+function removerItem(item) {
+  carrinho.value.items.quantidade = 1
+  carrinho.value.items.splice(carrinho.value.items.indexOf(item), 1)
+  produtos.value[item.id - 1].adicionado = false
 }
 
 function adicionarItem(item) {
@@ -20,6 +24,7 @@ function adicionarItem(item) {
     itemCarrinho.quantidade += 1
   } else {
     carrinho.value.items.push(item)
+    carrinho.value.items.item.quantidade++
   }
 }
 
@@ -33,45 +38,84 @@ function decrementaCompra(index) {
   } else index.value.quantidade--
 }
 
+const total = ref(0.0)
 
-// Fazer a função de valor total e mexer no css e template// 
+function calcularTotal() {
+  total.value = 0.0
+  for (let item of carrinho.value.items) {
+    total.value += item.quantidade * item.details.price
+  }
+  return (total.value = total.value.toFixed(2))
+}
 
 </script>
 <template>
-  <main>
-    <ul>
-      <li class="container" v-for="item in produtos" :key="item.id">
+  <header>
+    <div class="container">
+      <button
+        @click="mostrar = !mostrar"
+        class="btn btn-warning"
+      >
+        Mostrar Carrinho
+      </button>
+
+      <div v-if="mostrar">
+        <div>
+          <h1>CARRINHO</h1>
+          <p>O valor total é de R$: {{ calcularTotal() }}</p>
+        </div>
+        <button class="btn btn-outline-success" @click="limparCarrinho()">Limpar Carrinho</button>
         <div class="row">
-          <div class="col">
-            <div class="card">
-              <h5>Nome: {{ item.name }}</h5>
-              <p>detalhes: {{ item.details.description }}</p>
-              <p>price:R$:{{ item.details.price }}</p>
-              <button class="" @click="incrementaCompra(ref(item))">+</button>
-              <button @click="decrementaCompra(ref(item))">-</button>
-              <button @click="adicionarItem(item)">Adicionar item</button>
-              <p>{{ item.quantidade }}</p>
-              <img src="" alt="" />
+          <div class="col-4" v-for="(index, item) in carrinho.items" :key="item.id">
+            <div id="cardCarrinho" class="card">
+              <p>{{ index.name }}, R$:{{ index.details.price }}</p>
+              <p>A quantidade adicionada do item é: {{ index.quantidade }}</p>
+              <button class="btn btn-warning" @click="incrementaCompra(ref(index))">+</button>
+              <button class="btn btn-danger" @click="decrementaCompra(ref(index))">-</button>
+              <button class="btn btn-warning" @click="removerItem(item)">Remover item</button>
             </div>
           </div>
         </div>
-      </li>
-    </ul>
-
-    <div>
-      <button @click="mostrar =! mostrar">Mostrar Carrinho</button>
-    <div v-if="mostrar">
-      <button @click="limparCarrinho()">Limpar Carrinho</button>
-      
-      <div v-for="(index, item) in carrinho.items" :key="item.id">
-        {{ index }}
-        <button @click="incrementaCompra(ref(index))">+</button>
-        <button @click="decrementaCompra(ref(index))">-</button>
-    
-     
       </div>
     </div>
-  </div>
+  </header>
+
+  <main>
+    <div class="container">
+      <div><h1>ITENS</h1></div>
+      <div class="row">
+        <div class="col-4" v-for="item in produtos" :key="item.id">
+          <div id="card-inicial" class="card">
+            <h5>{{ item.name }}</h5>
+            <p>{{ item.details.description }}</p>
+            <p>price:R$:{{ item.details.price }}</p>
+            <button class="btn btn-success" @click="incrementaCompra(ref(item))">+</button>
+            <button class="btn btn-primary" @click="decrementaCompra(ref(item))">-</button>
+            <button class="btn btn-success" @click="adicionarItem(item)">Adicionar item</button>
+          </div>
+        </div>
+      </div>
+    </div>
   </main>
 </template>
-<style scoped></style>
+<style scoped>
+#card-inicial {
+  background-color: yellow;
+}
+
+#cardCarrinho {
+  background-color: green;
+}
+
+h5 {
+  font-weight: bold;
+}
+p {
+  font-weight: bold;
+}
+
+h1 {
+  text-align: center;
+  margin: 0%;
+}
+</style>
--- src/_data/produtos.js
+++ src/_data/produtos.js
@@ -5,40 +5,40 @@ const produtos = ref([
     id: 1,
     name: 'Camisa',
     details: { description: 'Camisa Social do Bolsonaro', price: 50.0 },
-    quantidade: 0,
+    quantidade: 1,
    
   },
   {
     id: 2,
     name: 'Calça',
     details: { description: 'Calça super confortavel do Mito', price: 20.0 },
-    quantidade: 0,
+    quantidade: 1,
    
   },
   { id: 3,
     name: 'Meia',
     details: { description: 'Meia Patriota', price: 20.0 },
-    quantidade: 0,
+    quantidade: 1,
     
       },
   { id: 4,
     name: 'Caneca',
     details: { description: 'Caneca DO MITO', price: 20.0 },
-    quantidade: 0,
+    quantidade: 1,
     
        },
   {
     id: 5,
     name: 'Calendario',
     details: { description: 'Calendario do Capitão', price: 20.0 },
-    quantidade: 0,
+    quantidade: 1,
    
   },
   { 
     id: 6,
     name: 'Toalha',
     details: { description: 'Toalha Patriota', price: 20.0 },
-    quantidade: 0,
+    quantidade: 1,
      },
 ])
 
